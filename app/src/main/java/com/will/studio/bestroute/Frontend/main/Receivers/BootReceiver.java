f8d59881f396a9c5697c563d46664440745c6aa4
Will1229 <gaozhixiang@gmail.com>
2016-02-01 16:43:55 +0100

    restore alarms after restart

--- app/src/main/java/com/will/studio/bestroute/Frontend/main/Receivers/BootReceiver.java
+++ app/src/main/java/com/will/studio/bestroute/Frontend/main/Receivers/BootReceiver.java
@@ -4,15 +4,29 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import com.will.studio.bestroute.backend.RouteDataManager;
+import com.will.studio.bestroute.backend.RouteItem;
+import com.will.studio.bestroute.frontend.main.RouteAlarmScheduler;
+
+import java.util.ArrayList;
+
 /**
  * Created by egaozhi on 2016-01-07.
- *
+ * Project: BestRoute
  */
 public class BootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
-            // TODO: Set the alarm here.
+            RouteDataManager routeDataManager = new RouteDataManager(context.getFilesDir()
+                    .getAbsolutePath());
+            routeDataManager.restoreAllItemsFromDisc();
+            ArrayList<RouteItem> routeItemList = routeDataManager.getAllItems();
+            RouteAlarmScheduler routeAlarmScheduler = new RouteAlarmScheduler(context);
+            for (RouteItem item : routeItemList
+                    ) {
+                routeAlarmScheduler.scheduleAlarm(item);
+            }
         }
     }
 }
